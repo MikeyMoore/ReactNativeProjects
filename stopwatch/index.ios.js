b5bebe0f68a51a9efb1b1b0c6ffa5e7ac5580f1e
MikeyMoore <icarusjedi@gmail.com>
2017-02-22 20:20:09 -0800

    layout of stopwatch with border colors

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,1 +1,67 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry
+} from 'react-native';
 
+var StopWatch = React.createClass({
+  render: function(){
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={this.border('red')}>
+          <Text>
+            00:00.00
+          </Text>
+        </View>
+        <View style={this.border('green')}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer, this.border('blue')]}>
+        <Text>
+          I am a list of laps
+        </Text>
+      </View>
+
+    </View>
+  },
+  startStopButton: function() {
+    return <View>
+      <Text>
+        Start
+      </Text>
+    </View>
+  },
+  lapButton: function() {
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  },
+  border: function(color){
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems:'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
