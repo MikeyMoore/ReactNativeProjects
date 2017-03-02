2ac1886278f024f24fc7b5870bfe70e2d0824e27
MikeyMoore <icarusjedi@gmail.com>
2017-03-01 16:30:20 -0800

    slider table

--- Table Views/Table Views/ViewController.swift
+++ Table Views/Table Views/ViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        
+        table.reloadData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell (style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value * 20)
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
+    
+    }
+    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//    
+//        let cell = UITableViewCell (style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+//        
+//        cell.textLabel?.text = "Test"
+//        
+//        return cell
+//    
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
