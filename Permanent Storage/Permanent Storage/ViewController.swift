3180c2217412f40df03aa49dd5e6b0d62b3d1883
MikeyMoore <icarusjedi@gmail.com>
2017-03-03 01:02:23 -0800

    create functionality to store number of clicks

--- Permanent Storage/Permanent Storage/ViewController.swift
+++ Permanent Storage/Permanent Storage/ViewController.swift
@@ -10,27 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    @IBAction func addDay(_ sender: Any) {
+        
+        let progress = UserDefaults.standard.object(forKey: "progress")!
+        
+        let skillCount = (progress as! Int) + 1
+        
+        UserDefaults.standard.set(skillCount, forKey: "progress")
+        
+        updateSkillLabel(totalCount: skillCount)
+        
+//        print("progress")
+//        
+//        print(progress)
+        
+        
+    }
+    
+    func updateSkillLabel(totalCount: Int) {
+        progressLabel.text! = "\(totalCount) times trained"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        UserDefaults.standard.set("Mike", forKey: "name")
+//        UserDefaults.standard.set(0, forKey: "progress")
         
-        let userName = UserDefaults.standard.object(forKey: "name")!
+        let progress = UserDefaults.standard.object(forKey: "progress")!
         
-        print(userName)
+        updateSkillLabel(totalCount: progress as! Int)
+
         
-        let arr = [1,2,3,4]
         
-        UserDefaults.standard.set(arr, forKey: "array")
         
-        let returnedArray = UserDefaults.standard.object(forKey: "array")! as! NSArray
         
-        for x in returnedArray {
-        
-            print(x)
-        
-        }
+//        let arr = [1,2,3,4]
+//        
+//        UserDefaults.standard.set(arr, forKey: "array")
+//        
+//        let returnedArray = UserDefaults.standard.object(forKey: "array")! as! NSArray
+//        
+//        for x in returnedArray {
+//        
+//            print(x)
+//        
+//        }
         
     }
 
